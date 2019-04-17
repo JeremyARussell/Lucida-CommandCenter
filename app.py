f5232a22dab44280228ee80e26f0a625d540c2c5
Jeremy Russell <jeremyarussell@gmail.com>
2019-04-17 16:37:43 -0700

    messed up a rebase and had to manually erase a bunch of empty lines created during the 2to3 process.

--- app.py
+++ app.py
@@ -1,71 +1,69 @@
-
-
-import glob
-import logging
-import os
-import sys
-from threading import Thread
-
-from flask import Flask, Blueprint
-
-from controllers import Main, User, Create, Learn, Infer
-from controllers.Parser import cmd_port
-from controllers import WebSocket
-
-sys.path.insert(0, glob.glob(os.path.abspath(os.path.dirname(__file__)) +
-    '/thrift-0.9.3/lib/py/build/lib*')[0])
-
-
-
-# Initialize the Flask app with the template folder address.
-app = Flask(__name__, template_folder='templates')
-
-# app.config.from_object('config')
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 # 16 MB due to MongoDB
-
-# Register the controllers.
-app.register_blueprint(Main.main)
-app.register_blueprint(User.user)
-app.register_blueprint(Create.create)
-app.register_blueprint(Learn.learn)
-app.register_blueprint(Infer.infer)
-
-# Session.
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'# TODO - Any secret key ever should not be in the source code, this will need to be turned into configuration
-
-def flask_listener():
-
-    # For https (with ASR capability)
-    if os.environ.get('SECURE_HOST'):
-        print ('Starting secure flask')
-        app.run(host='0.0.0.0', port=3000, debug=True, use_reloader=False,
-                threaded=True, ssl_context=('certs/server.crt', 'certs/server.key'))
-    # For http (without ASR capability)
-    else:
-        print ('Starting non-secure flask')
-        app.run(host='0.0.0.0', port=3000, debug=True, use_reloader=False,
-                threaded=True)
-
-def web_socket_listener():
-    print(('Start web socket at ' + str(cmd_port)))
-    logging.basicConfig(level=logging.DEBUG,
-            format="%(levelname)8s %(asctime)s %(message)s ")
-    logging.debug('Starting up server')
-    WebSocket.tornado.options.parse_command_line()
-
-    # For wss (with ASR capability)
-    if os.environ.get('SECURE_HOST'):
-        print ('Starting secure web socket')
-        WebSocket.Application().listen(cmd_port, ssl_options={
-            "certfile":"certs/server.crt",
-            "keyfile":"certs/server.key"})
-    # For ws (without ASR capability)
-    else:
-        print ('Starting non-secure web socket')
-        WebSocket.Application().listen(cmd_port)
-
-    WebSocket.tornado.ioloop.IOLoop.instance().start()
-
-if __name__ == '__main__':
-    Thread(target = flask_listener).start()
-    web_socket_listener()
+import glob
+import logging
+import os
+import sys
+from threading import Thread
+
+from flask import Flask, Blueprint
+
+from controllers import Main, User, Create, Learn, Infer
+from controllers.Parser import cmd_port
+from controllers import WebSocket
+
+sys.path.insert(0, glob.glob(os.path.abspath(os.path.dirname(__file__)) +
+    '/thrift-0.9.3/lib/py/build/lib*')[0])
+
+
+
+# Initialize the Flask app with the template folder address.
+app = Flask(__name__, template_folder='templates')
+
+# app.config.from_object('config')
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 # 16 MB due to MongoDB
+
+# Register the controllers.
+app.register_blueprint(Main.main)
+app.register_blueprint(User.user)
+app.register_blueprint(Create.create)
+app.register_blueprint(Learn.learn)
+app.register_blueprint(Infer.infer)
+
+# Session.
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'# TODO - Any secret key ever should not be in the source code, this will need to be turned into configuration
+
+def flask_listener():
+
+    # For https (with ASR capability)
+    if os.environ.get('SECURE_HOST'):
+        print ('Starting secure flask')
+        app.run(host='0.0.0.0', port=3000, debug=True, use_reloader=False,
+                threaded=True, ssl_context=('certs/server.crt', 'certs/server.key'))
+    # For http (without ASR capability)
+    else:
+        print ('Starting non-secure flask')
+        app.run(host='0.0.0.0', port=3000, debug=True, use_reloader=False,
+                threaded=True)
+
+def web_socket_listener():
+    print(('Start web socket at ' + str(cmd_port)))
+    logging.basicConfig(level=logging.DEBUG,
+            format="%(levelname)8s %(asctime)s %(message)s ")
+    logging.debug('Starting up server')
+    WebSocket.tornado.options.parse_command_line()
+
+    # For wss (with ASR capability)
+    if os.environ.get('SECURE_HOST'):
+        print ('Starting secure web socket')
+        WebSocket.Application().listen(cmd_port, ssl_options={
+            "certfile":"certs/server.crt",
+            "keyfile":"certs/server.key"})
+    # For ws (without ASR capability)
+    else:
+        print ('Starting non-secure web socket')
+        WebSocket.Application().listen(cmd_port)
+
+    WebSocket.tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == '__main__':
+    Thread(target = flask_listener).start()
+    web_socket_listener()
